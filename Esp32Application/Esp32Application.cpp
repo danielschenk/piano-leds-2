97ec17dde6bd947aa529ae63ee5af8492cfd079a
Daniël Schenk <danielschenk@gmail.com>
2025-09-12 19:53:21 +0200

    chore: remove unused testing patches
    these became obsolete when introducing the legacy patches

--- Esp32Application/Esp32Application.cpp
+++ Esp32Application/Esp32Application.cpp
@@ -1,15 +1,10 @@
 #include "ArduinoMidiInput.hpp"
 #include "ConcertInfrastructure.hpp"
-#include "IPatch.hpp"
-#include "LinearRgbFunction.hpp"
 #include "Logging.hpp"
 #include "LoggingTask.hpp"
 #include "MidiMessageLogger.hpp"
 #include "MidiTask.hpp"
-#include "NoteVisualizer.hpp"
-#include "PianoDecayRgbFunction.hpp"
 #include "ProcessingTask.hpp"
-#include "SingleColorFill.hpp"
 #if PIANOLEDS_NEOPIXEL
 #include "LedTaskNeoPixel.hpp"
 #else
@@ -22,34 +17,15 @@
 
 #define LOGGING_COMPONENT "Esp32Application"
 
-static MidiTask* midiTaskPtr(nullptr);
+static MidiTask* midiTaskPtr{nullptr};
 
 static constexpr uint32_t defaultStackSize(4096);
 
 enum
 {
-    /**
-     * Idle priority level.
-     */
     PRIORITY_IDLE = 0,
-
-    /**
-     * Priority level for everything which is least important.
-     * @note Same as the task which runs the setup/loop functions.
-     * Examples: logging
-     */
     PRIORITY_LOW = 1,
-
-    /**
-     * Priority level for things which are not critical, but should not be delayed by logging.
-     * Examples: web UI for configuration
-     * */
     PRIORITY_UI = 2,
-
-    /**
-     * Priority level for the main tasks of the device which should always have priority.
-     * Examples: MIDI, processing, SPI
-     */
     PRIORITY_CRITICAL = 3
 };
 
@@ -78,76 +54,15 @@ void setup()
     static MidiMessageLogger midiMessageLogger(midiInput);
 
     static application::ConcertInfrastructure concertInfrastructure(midiInput, freeRtosTime);
-
-    // TODO read concert from storage
-    // For now, add something to test with.
-    auto& concert = concertInfrastructure.concert;
-    IPatch* patch(concert.getPatch(concert.addPatch()));
-    patch->setName("whiteOnBlue");
-    patch->setBank(0);
-    patch->setProgram(19);
-
-    // Add constant blue background
-    auto src1(new SingleColorFill);
-    src1->setColor(processing::RgbColor({0, 0, 32}));
-    patch->getProcessingChain().insertBlock(src1);
-
-    // Full white for any sounding key
-    auto src2(
-        new NoteVisualizer(midiInput, concertInfrastructure.rgbFunctionFactory, freeRtosTime));
-    auto rgbFunction(std::make_shared<LinearRgbFunction>());
-    const processing::LinearConstants fullWhite({255, 0});
-    rgbFunction->setRedConstants(fullWhite);
-    rgbFunction->setGreenConstants(fullWhite);
-    rgbFunction->setBlueConstants(fullWhite);
-    src2->setRgbFunction(rgbFunction);
-    src2->setUsingPedal(true);
-    patch->getProcessingChain().insertBlock(src2);
-    patch->activate();
-
-    // Add another patch
-    IPatch* patch2(concert.getPatch(concert.addPatch()));
-    auto src3(
-        new NoteVisualizer(midiInput, concertInfrastructure.rgbFunctionFactory, freeRtosTime));
-
-    // Sounding notes become blue, intensity is the velocity of the note multiplied by 2
-    rgbFunction = std::make_shared<LinearRgbFunction>();
-    rgbFunction->setBlueConstants({2, 0});
-    src3->setRgbFunction(rgbFunction);
-
-    src3->setUsingPedal(true);
-
-    patch2->getProcessingChain().insertBlock(src3);
-    patch2->setName("blueNote");
-    patch2->setBank(0);
-    patch2->setProgram(20);
-
-    // Add another patch
-    IPatch* patch3(concert.getPatch(concert.addPatch()));
-
-    // Red background, white notes, mimic piano
-    auto src4(new SingleColorFill);
-    src4->setColor({32, 0, 0});
-    patch3->getProcessingChain().insertBlock(src4);
-
-    auto src5(
-        new NoteVisualizer(midiInput, concertInfrastructure.rgbFunctionFactory, freeRtosTime));
-
-    auto fnc(std::make_shared<PianoDecayRgbFunction>());
-    src5->setRgbFunction(fnc);
-    src5->setUsingPedal(true);
-    patch3->getProcessingChain().insertBlock(src5);
-    patch3->setName("whitePianoNotes");
-    patch3->setBank(0);
-    patch3->setProgram(21);
-
-    static ProcessingTask processingTask(concert, defaultStackSize, PRIORITY_CRITICAL);
+    static ProcessingTask processingTask(concertInfrastructure.concert, defaultStackSize,
+                                         PRIORITY_CRITICAL);
 
 #if PIANOLEDS_NEOPIXEL
-    static LedTaskNeoPixel ledTask(concert, LED_DATA_PIN, defaultStackSize, PRIORITY_CRITICAL);
+    static LedTaskNeoPixel ledTask(concertInfrastructure.concert, LED_DATA_PIN, defaultStackSize,
+                                   PRIORITY_CRITICAL);
 #else
-    static LedTask ledTask(concert, LED_DATA_PIN, LED_CLOCK_PIN, defaultStackSize,
-                           PRIORITY_CRITICAL);
+    static LedTask ledTask(concertInfrastructure.concert, LED_DATA_PIN, LED_CLOCK_PIN,
+                           defaultStackSize, PRIORITY_CRITICAL);
 #endif
 
     if (false)
@@ -164,20 +79,17 @@ static void startNetwork()
 
 void loop()
 {
-    static unsigned s_loopCount(0);
+    static unsigned loopCount(0);
 
-    // Nothing to do, leave everything to the other tasks.
     vTaskDelay(1000);
-
-    // Blink to indicate we're alive.
     digitalWrite(RUN_LED_PIN, !digitalRead(RUN_LED_PIN));
 
-    if ((s_loopCount % (5 * 60)) == 0)
+    if ((loopCount % (5 * 60)) == 0)
     {
         LOG_INFO_PARAMS("free heap: %u", ESP.getFreeHeap());
     }
 
-    ++s_loopCount;
+    ++loopCount;
 }
 
 void serialEvent2()
