3b4411d2e0d5a73caae3d5e1548df1fa0f809539
Daniël Schenk <danielschenk@gmail.com>
2025-12-20 11:41:16 +0100

    fix: OnOffRgbFunction didn't use press down color

--- lib/Processing/OnOffRgbFunction.cpp
+++ lib/Processing/OnOffRgbFunction.cpp
@@ -8,9 +8,16 @@ OnOffRgbFunction::OnOffRgbFunction(const RgbColor& color) : color(color) {}
 RgbColor OnOffRgbFunction::calculate(const NoteState& noteState, Timestamp currentTime) const
 {
     if (noteState.sounding)
-        return color;
+    {
+        if (noteState.pressDownColor != color_constants::off)
+            return noteState.pressDownColor;
+        else
+            return color;
+    }
     else
+    {
         return color_constants::off;
+    }
 }
 
 }  // namespace processing
