7175c03a1b7525853784b982dc32f67f00dd6703
Daniël Schenk <danielschenk@gmail.com>
2025-12-29 14:16:48 +0100

    remove class which was redone on main

--- lib/Processing/ChordTriggeredAttackReleaseFilter.cpp
+++
@@ -1,8 +0,0 @@
-#include "ChordTriggeredAttackReleaseFilter.hpp"
-
-void ChordTriggeredAttackReleaseFilter::execute(processing::RgbStrip& strip, const Input& input) {}
-
-ProcessingBlock::Mode ChordTriggeredAttackReleaseFilter::mode() const
-{
-    return Mode::overwriting;
-}
